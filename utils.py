c7ef939e27a674c8e997330c1dd6c43072ab14a6
Ricky <uhrishabh@gmail.com>
2023-01-26 18:11:33 +0100

    Changes for retriving results and making result file.

--- utils.py
+++ utils.py
@@ -3,7 +3,9 @@ import os
 import random
 import numpy as np
 from tqdm import tqdm
-
+from sklearn.metrics import roc_curve, auc
+import pandas as pd
+import glob
 
 def set_seed(seed):
     """ Set all seeds to make results reproducible """
@@ -31,3 +33,57 @@ def evaluate_loss(net, device, criterion, dataloader):
             count += 1
 
     return mean_loss / count
+
+
+
+
+def Find_Optimal_Cutoff(target, predicted):
+    """ Find the optimal probability cutoff point for a classification model related to event rate
+    Parameters
+    ----------
+    target : Matrix with dependent or target data, where rows are observations
+
+    predicted : Matrix with predicted data, where rows are observations
+
+    Returns
+    -------
+    list type, with optimal cutoff value
+
+    """
+    fpr, tpr, threshold = roc_curve(target, predicted)
+    i = np.arange(len(tpr))
+    roc = pd.DataFrame({'tf': pd.Series(tpr - (1 - fpr), index=i), 'threshold': pd.Series(threshold, index=i)})
+    roc_t = roc.iloc[(roc.tf - 0).abs().argsort()[:1]]
+
+    return list(roc_t['threshold'])
+
+
+def reading_evaluator(folder_path):
+    result_folder=folder_path+"/result/"
+    res_c=[]
+    res=[]
+    csv_files = glob.glob(folder_path + "/**/*.csv", recursive=True)
+    for csv_file in csv_files:
+        if 'result' not in csv_file.split("/")[-2]:
+            if 'c_score' in csv_file.split("/")[-2]:
+                batch=int(csv_file.split("/")[-2].split("_")[2])
+                epochs=int(csv_file.split("/")[-2].split("_")[3])
+                score_df=pd.read_csv(csv_file).tail(1)['MRR@10'].values
+                if score_df>0:
+                    score=score_df[0]
+                    res_c.append([batch,epochs,score])
+
+            else:
+                batch=int(csv_file.split("/")[-2].split("_")[2])
+                epochs=int(csv_file.split("/")[-2].split("_")[3])
+                score_df = pd.read_csv(csv_file).tail(1)['MRR@10'].values
+                if score_df>0:
+                    score = score_df[0]
+                    res.append([batch,epochs,score])
+
+    pd.DataFrame(res_c, columns=['epochs', 'batch', 'MRR@10']).to_csv(result_folder + "c_score.csv", index=None, sep=';')
+    pd.DataFrame(res,columns=['epochs','batch','MRR@10']).to_csv(result_folder+"no_ccscore.csv",index=None,sep=';')
+
+
+# folder_path='/tmp/pycharm_project_447/cross_encoder_CRRerank_bert_base'
+# reading_evaluator(folder_path)
